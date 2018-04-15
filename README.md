334986ab848e41e3b20589240e6e4d20d78b6060
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2018-04-14 21:42:49 -0400

    Init with bare scripts

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# flow
+# <sup><sub><sup><sub>[ebb &]</sub></sup></sub></sup> Flow
+
 Experiment pipeline
+
+TODO
+
+# Tools
+
+## Deploy code to singularity-hub
+
+Include it in .travis.yml files for automatic deployment after successful tests.
+
+```bash
+flow-deploy ...
+```
+
+## Submit jobs on clusters
+
+Using SmartDispatch as a backend
+
+```bash
+flow-submit [SCHEDULER OPTIONS] [COMMAND] ...
+```
+
+Can be combined with `flow-execute`
+
+```bash
+flow-submit [SCHEDULER OPTIONS] flow-execute shub://bouthilx/my-experiment:version [COMMAND] ...
+```
+
+## Execute jobs using singularity containers
+
+```bash
+flow-execute shub://bouthilx/my-experiment:version [COMMAND] ...
+```
+
+## Render results
+
+```bash
+flow-analyze
+```
+
+```bash
+flow-compare
+```
+
+# Install
+
+TODO
+
+# Configuration
+
+TODO
---
+++ setup.py
@@ -0,0 +1,42 @@
+"""Installation script."""
+from os import path
+from io import open
+from setuptools import find_packages, setup
+
+HERE = path.abspath(path.dirname(__file__))
+
+def readme():
+    with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
+        return f.read().strip()
+
+
+tests_require = [
+    'pytest>=3.0.0'
+    ]
+
+setup(
+    name='flow',
+    version=0.1,
+    description='Experiment pipeline',
+    long_description=readme(),
+    url='https://github.com/bouthilx/flow.git',
+    author='Xavier Bouthillier',
+    license='MIT',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+    tests_require=tests_require,
+    extras_require=dict(test=tests_require),
+    scripts=[
+        'bin/flow-deploy',
+        'bin/flow-submit',
+        'bin/flow-execute'
+        ],
+    entry_points={
+        'console_scripts': [
+            'flow-compare = flow.bin.compare:main',
+            'flow-analyze = flow.bin.analyze:main'
+            ]
+        },
+    zip_safe=False
+)
